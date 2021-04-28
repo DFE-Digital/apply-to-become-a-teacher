6ae900f82416bdb74e83d33587fe4a2c44d96ef4
David Gisbey <davidgisbey@gmail.com>
2021-04-28 16:41:20 +0100

    Stop ApplyAgain service from duping the application form if at max choices

--- app/services/apply_again.rb
+++ app/services/apply_again.rb
@@ -4,8 +4,23 @@ class ApplyAgain
   end
 
   def call
+    return false if application_choices_with_accepted_states.count >= 5
+
     DuplicateApplication.new(@application_form, target_phase: 'apply_2')
       .duplicate
       .tap(&:mark_sections_incomplete_if_review_needed!)
   end
+
+ private
+
+ def application_choices_with_accepted_states
+   @application_form
+     .candidate
+     .application_forms
+     .includes(application_choices: [:course, :site, :provider, :current_course, :current_course_option, :interviews])
+     .map(&:application_choices)
+     .flatten
+     .sort_by(&:id)
+     .select { |ac| ac.status.to_sym.in?(ApplicationStateChange::ACCEPTED_STATES + ApplicationStateChange::INTERVIEWABLE_STATES + [:offer]) }
+ end
 end
