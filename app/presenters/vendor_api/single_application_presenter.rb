7de60393d3a16ce5ba41c6af0845f5b5e22d5791
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2021-05-16 00:56:05 +0100

    Ensure that the new selected bool is backwards compatible
    This commit ensures that selected is set to true when the reference is
    received. It also scopes the references used in the provider UI and
    Vendor API are scoped to selected being true.

--- app/presenters/vendor_api/single_application_presenter.rb
+++ app/presenters/vendor_api/single_application_presenter.rb
@@ -187,7 +187,7 @@ module VendorAPI
     end
 
     def references
-      application_form.application_references.feedback_provided.map do |reference|
+      application_form.application_references.feedback_provided.where(selected: true).map do |reference|
         reference_to_hash(reference)
       end
     end
